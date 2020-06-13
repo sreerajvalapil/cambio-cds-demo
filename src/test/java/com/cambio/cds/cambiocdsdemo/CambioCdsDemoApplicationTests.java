83cf9cb8f9c31560452ec9469fc8e44677caf128
sreeraj <sree.valapil@gmail.com>
2020-06-13 06:54:25 +0200

    Initial project set up

---
+++ src/test/java/com/cambio/cds/cambiocdsdemo/CambioCdsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambio.cds.cambiocdsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioCdsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
